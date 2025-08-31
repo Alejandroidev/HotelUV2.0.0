c3afc560dcf5165699a10ac3866d0465b5ae87bf
Alejandro Salcedo <98795415+Alejandroidev@users.noreply.github.com>
2025-08-31 18:55:17 -0500

    git ignore

--- Common/obj/Debug/net9.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net9.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98679cbce470ff9eeaea3e877661d4326b425a19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21243e6e9e41229089ab41167f04aea2dad2c67d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
